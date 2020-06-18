9e078b1de59455c5de40f5c536ed6240e69d0ec5
Ben Saini <bensaini7@gmail.com>
2020-06-18 04:44:35 -0400

    Create today's file for the lesson.

---
+++ examples/June_18_Lesson.py
@@ -0,0 +1,19 @@
+
+# Given names of a few people ["Ben", "Devika", "Shivani", "Stuti"]. Do
+# the following 4 questions. Feel free to invent values for the dictionary.
+#
+# 1. For the above mentioned names of people write a dictionary that
+# stores the cellphone number of the people by name
+#
+# 2. For the above mentioned names of people write a dictionary that
+# stores the age of the people by name
+#
+# 3. For the above mentioned names of people write a dictionary that
+# stores the address of the people by name
+#
+#
+# 4. Then write a loop, you are free to use for or while, to print
+# age, address, and cellphone numbers.
+
+cellphone_by_name = {}
+cellphone_by_name["Ben"] =  "+1 (516) 661 - 5678"
---
+++ lessons/git_lesson.md
@@ -0,0 +1,10 @@
+
+# Lesson June 18th, 2020
+
+# Concept Git command line 
+    
+    git clone URL
+    git add . (or filename)
+    
+    
+# Concept command prompt - cd, dir, 
