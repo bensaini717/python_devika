c2e4b4ed2a17264b0ec48446a53a6db605fec552
Devika Saini <sainidevika12@gmail.com>
2020-06-26 14:18:09 +0530

    update

--- examples/main.py
+++ examples/main.py
@@ -21,3 +21,26 @@ circle_object["Three"] = [56, "yellow"]
 circle_object["Four"] = [12, "pink"]
 circle_object["Five"] = [15, "orange"]
 
+print(len(circle_object))
+
+list_of_keys = list(circle_object.keys())
+print(list_of_keys[0])
+print(list_of_keys)
+
+counter = 0
+
+while counter < len(list_of_keys):
+    key = list_of_keys[counter]
+    print(key)
+    value = circle_object[key]
+    radius = value[0]
+    colour = value[1]
+    circle_area = area_svc_obj.area_Circle(radius = radius)
+    paint_task_resp = paint_svc_obj.paint_Circle(area=circle_area, color=colour)
+    print(paint_task_resp)
+
+
+    # print("Key is {} and radius is {} and colour is {}".format(key,radius,colour))
+    counter = counter + 1
+
+print("thanks")
